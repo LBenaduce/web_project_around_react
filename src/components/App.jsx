7e4d22c77d2f3ffa241299a1946ffe421556611c
LBenaduce <luiz.benaduce@gmail.com>
2026-01-30 14:11:34 -0300

    Sprint 13: React base, popups and cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
 
 export default function App() {
   return (
-    <div className="page__content">
+    <div className="page">
       <Header />
       <Main />
       <Footer />
