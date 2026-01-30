27ac2555eae1fe826109f0f3b799ae6b79fc94f2
LBenaduce <luiz.benaduce@gmail.com>
2026-01-30 11:02:58 -0300

    Sprint 13: React base, popups and cards

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="footer page__section">
-      <p className="footer__copyright">© 2026 Around The U.S.</p>
+      <p className="footer__copyright">Luiz Benaduce © 2026 Around The U.S.</p>
     </footer>
   );
 }
