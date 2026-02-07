8cdb900d39a46d1511c52fa74414eb8e9ec91a11
LBenaduce <luiz.benaduce@gmail.com>
2026-02-07 16:24:50 -0300

    chore: remove duplicate and unused components

--- src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function EditAvatar() {
-  return (
-    <form className="popup__form">
-      <input placeholder="Avatar link" />
-      <button type="submit">Salvar</button>
-    </form>
-  );
-}
